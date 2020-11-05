ce1fbece138eaf592ba79f0607eaee21dc192b47
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2020-11-05 09:07:35 +0300

    Добавил меню

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './Layout.module.css'
 import MenuToggle from "../Navigation/MenuToggle/MenuToggle";
+import Menu from "../Navigation/Menu/Menu";
 
 class Layout extends Component
 {
@@ -12,14 +13,26 @@ class Layout extends Component
             menuIsOpen: !this.state.menuIsOpen
         })
     }
+
+    backdropCloseHandler = () => {
+        this.setState({
+            menuIsOpen: false
+        })
+    }
+
     render() {
+
         return (
             <div className={classes.Layout}>
                 <MenuToggle
                     onMenuToggleHandler={this.onMenuToggleHandler}
                     menuIsOpen={this.state.menuIsOpen}
                 />
-                <main>
+                <Menu
+                    isOpen={this.state.menuIsOpen}
+                    backdropClose={this.backdropCloseHandler}
+                />
+                <main className={classes.Layout__main}>
                     {this.props.children}
                 </main>
             </div>
