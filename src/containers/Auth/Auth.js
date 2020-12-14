67a848ffcdf83617ee31bf673d2bc6de01815a65
Egorov Dmitriy <d.egorov@bop24.ru>
2020-12-14 13:57:49 +0300

    Верстка страницы auth и создание компонента Input

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
 import classes from './Auth.module.css';
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export default class Auth extends Component {
+    loginHandler = () => {}
+    registerHandler = () => {}
+    submitHandler = e => {
+        e.preventDefault();
+    }
     render() {
         return (
-            <React.Fragment>
-                <h1>Auth</h1>
-            </React.Fragment>
+            <div className={classes.Auth}>
+                <div className={classes.AuthWrapper}>
+                    <h1>Авторизация</h1>
+                    <form className={classes.AuthForm} onSubmit={this.submitHandler}>
+                        <Input
+                            label='Email'
+                        />
+                        <Input
+                            label='Пароль'
+                        />
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                        >Войти</Button>
+                        <Button
+                            type="primary"
+                            onClick={this.registerHandler}
+                        >Зарегистрироваться</Button>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
